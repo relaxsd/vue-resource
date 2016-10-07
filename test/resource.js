b08aa9636aa6f4166b79dd1ab200c5a4c7d34bfc
Martijn Kriek <martijn.kriek@relax-software.nl>
2016-10-07 21:46:49 +0200

    Fixed typo in exception message (+testcase).

--- test/resource.js
+++ test/resource.js
@@ -25,6 +25,29 @@ describe('this.$resource', function () {
 
     });
 
+    it('get({file: "valid.json"}) with too many arguments', (done) => {
+
+        var vm = new Vue({
+
+            created() {
+
+                var resource = this.$resource('data{/file}');
+
+                try {
+                    resource.get({file: 'valid.json'}, {}, {});
+
+                } catch (e) {
+                    expect(e).toBe('Expected up to 2 arguments [params, body], got 3 arguments');
+                    done();
+
+                }
+
+            }
+
+        });
+
+    });
+
     it('save({file: "valid.json"}, {foo: "bar"})', (done) => {
 
         var vm = new Vue({
