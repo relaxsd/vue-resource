d2014b1ea04f9cf042d0adea88c3033d02fa92a8
Martijn Kriek <martijn.kriek@relax-software.nl>
2016-10-08 11:46:54 +0000

    new release (still 1.3.0)

--- dist/vue-resource.common.js
+++ dist/vue-resource.common.js
@@ -786,7 +786,8 @@ function xdrClient (request) {
         };
 
         request.abort = function () {
-            return xdr.abort();
+            xdr.abort();
+            if (request.abortCb) request.abortCb(request);
         };
 
         xdr.open(request.method, request.getUrl());
@@ -1040,7 +1041,8 @@ function xhrClient (request) {
         };
 
         request.abort = function () {
-            return xhr.abort();
+            xhr.abort();
+            if (request.abortCb) request.abortCb(request);
         };
 
         if (request.progress) {
@@ -1139,6 +1141,119 @@ function sendRequest(request, resolve) {
 
     resolve(client(request));
 }
+
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
+
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
 
 var classCallCheck = function (instance, Constructor) {
   if (!(instance instanceof Constructor)) {
@@ -1412,25 +1527,28 @@ function Resource(url, params, actions, options) {
 
 function opts(action, args) {
 
-    var options = assign({}, action),
-        params = {},
+    var result = assign({}, action),
+        options = { params: {} },
         body;
 
     switch (args.length) {
 
         case 2:
 
-            params = args[0];
-            body = args[1];
+            // body, options
+            body = args[0];
+            assign(options, args[1]);
 
             break;
 
         case 1:
 
-            if (/^(POST|PUT|PATCH)$/i.test(options.method)) {
+            if (/^(POST|PUT|PATCH)$/i.test(result.method)) {
+                // For POST: single argument is body
                 body = args[0];
             } else {
-                params = args[0];
+                // For GET: single argument is options
+                assign(options, args[0]);
             }
 
             break;
@@ -1441,13 +1559,19 @@ function opts(action, args) {
 
         default:
 
-            throw 'Expected up to 4 arguments [params, body], got ' + args.length + ' arguments';
+            throw 'Expected up to 2 arguments [body, options], got ' + args.length + ' arguments';
     }
 
-    options.body = body;
-    options.params = assign({}, options.params, params);
+    result.body = body;
 
-    return options;
+    // Copy the passed {options:params} in result.params and delete them from the options
+    result.params = assign({}, result.params, options.params);
+    delete options.params;
+
+    // Add the rest of the {options} directly to the result
+    assign(result, options);
+
+    return result;
 }
 
 Resource.actions = {
@@ -1456,6 +1580,7 @@ Resource.actions = {
     save: { method: 'POST' },
     query: { method: 'GET' },
     update: { method: 'PUT' },
+    patch: { method: 'PATCH' },
     remove: { method: 'DELETE' },
     delete: { method: 'DELETE' }
 
